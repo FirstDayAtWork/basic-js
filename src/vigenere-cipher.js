350d03101debcf3a03e869a3aabb0ff14432e62f
Michael <fatlycka@gmail.com>
2024-12-27 02:18:24 +0300

    feat: did cipher task

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -20,13 +20,51 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  reveseIt(elem) {
+    return elem.toUpperCase().split``.reverse().join``;
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  getIndex(elem, str, key, i, j, type) {
+    if (type === 'en') {
+      return elem[elem.indexOf(str[i]) + elem.indexOf(key[j])];
+    }
+    if (type === 'de') {
+      return elem[elem.indexOf(str[i]) - elem.indexOf(key[j])];
+    }
+  }
+  constructor(reverse = true) {
+    this.reverse = reverse;
+    this.alphabet = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  }
+  encrypt(string, key) {
+    if (!string || !key) throw new Error("Incorrect arguments!");
+    let res = [] + [];
+    string = string.toLowerCase();
+    key = key.toLowerCase();
+    for (let i = +!{}, j = +![]; i < string.length; i += +!![], j += +!!{}) {
+      if (/[^a-zA-Z]/.test(string[i])) {
+        j -= +!!{};
+        res += string[i];
+        continue;
+      }
+      if (j > key.length - +!![]) j = +!{};
+      res += this.getIndex(this.alphabet, string, key, i, j, 'en');
+    }
+    return this.reverse ? res.toUpperCase() : this.reveseIt(res)
+  }
+  decrypt(string, key) {
+    if (!string || !key) throw new Error("Incorrect arguments!");
+    let res = [] + [];
+    key = key.toLowerCase();
+    for (let i = +!{}, j = +![]; i < string.length; i += +!![], j += +!!{}) {
+      if (/[^a-zA-Z]/.test(string[i])) {
+        j -= +!!{};
+        res += string[i];
+        continue;
+      }
+      if (j > key.length - +!![]) j = +!{};
+      res += this.getIndex(this.alphabet, string, key, i, j, 'de');
+    }
+    return this.reverse ? res.toUpperCase() : this.reveseIt(res)
   }
 }
 
